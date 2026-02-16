53aa491ebb5ad7d36da2033ab2c284aebaf741fc
muraliprakash100 <muraliprakash100@gmail.com>
2026-02-16 09:03:24 +0000

    Initial commit

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-// Add JS here
+var number = document.getElementById("inputBox")
+var answer = document.getElementById("validNbr")
+var randomNumber = Math.floor(Math.random() * 10)+1;
+
+if (randomNumber > 5) {
+    randomNumber = randomNumber - 5
+}
+
+function check() {
+    if (number.value == randomNumber) {
+        answer.innerHTML = "Well done! you have guessed the correct number"
+    }
+    else if (number.value == "") {
+       alert("Don't afried to enter a number")
+    }
+    else {
+        answer.innerHTML = "Better luck next time, the correct number is " + randomNumber;
+    }
+
+}
